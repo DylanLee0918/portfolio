0df30679f4362230e6f41a5badccc82c8fe9bd7f
Jed Dylan Lee <leedylan6058@gmail.com>
2021-05-04 15:50:09 +0800

    Test Styled Components

---
+++ src/components/Styled-Components/styled.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components'
+
+export const NavContainer = styled.div`
+    width: 100%;
+    height: 32px;
+    background-color: #FFF;
+    display: flex;
+    align-items: center;
+    border-bottom: 1px solid #92a8d1;
+`
+
+export const NavBrandContainer = styled.div`
+    border: 1px solid red;
+`
+
+export const NavBrandLogo = styled.img`
+
+`
